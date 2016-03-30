597089bea5aa22b7c2f56156b035787b08dcd412
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-30 16:48:33 -0700

    Changed data set generation for the chart to be generated by an object constructor. Graph displays but no data is shown...

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ var data = {};
 var allImageNames = [];
 var allTimesClicked = [];
 var allTimesShown = [];
+var datasets = [];
+var ctx;
+var createCanvasChart;
 
 // Accesses the imageObjectArray and returns a random value from that array
 function getRandomImage(){
@@ -23,39 +26,24 @@ function getDataArrays(){
     allTimesClicked.push(imageObjectArray[i].timesClicked);
     allTimesShown.push(imageObjectArray[i].timesShown);
   };
-  return allImageNames, allTimesClicked, allTimesShown;
-}
+};
 
 // Creates arrays with all of the relevant info to create chart (object names, times clicked, and times shown), creates datasets for chart, creates chart, and appends chart to page.
 function initializeChartData() {
   var data = {
     labels: allImageNames,
-    datasets: [
-      {
-        label: 'Times Clicked',
-        fillColor: 'rgba(220,220,220,0.5)',
-        highlightFill: 'rgba(220,220,220,0.75)',
-        highlightStroke: '#000000',
-        data: allTimesClicked
-      },
-      {
-        label: 'Times Shown',
-        fillColor: 'rgba(220,220,220,0.5)',
-        highlightFill: 'rgba(220,220,220,0.75)',
-        highlightStroke: '#000000',
-        data: allTimesShown
-      }
-    ]
+    datasets: [timesClickedDataSet, timesShownDataSet]
   };
-  var ctx = document.getElementById('canvasChart').getContext('2d');
+  console.table(data.datasets);
+  var ctx = document.getElementById('canvas-chart').getContext('2d');
   var createCanvasChart = new Chart(ctx).Bar(data);
 };
 
 // Adds an ID to each image on the page, reinitializes imagesOnPage so that new images will be added to the page, adds new images to the page, and reinitializes the event handler.
 function handleImageClick(event) {
   // Sets ID attributes of all images on page to the value of the image object's property name.
-  for (var i = 0; i < document.getElementsByClassName('researchImage').length; i++) {
-    document.getElementsByClassName('researchImage')[i].setAttribute('id', imageNamesOnPage[i]);
+  for (var i = 0; i < document.getElementsByClassName('research-image').length; i++) {
+    document.getElementsByClassName('research-image')[i].setAttribute('id', imageNamesOnPage[i]);
   }
   // Adds to the timesShown counter property for all the images on the page
   for (var i = 0; i < imageObjectsOnPage.length; i++) {
@@ -63,6 +51,7 @@ function handleImageClick(event) {
     // Adds to the timesClicked counter property specifically for the clicked image.
     if (event.target.id === imageObjectsOnPage[i].name) {
       imageObjectsOnPage[i].timesClicked++;
+      break;
     }
   }
   imageObjectsOnPage = [];
@@ -80,20 +69,20 @@ function handleImageClick(event) {
 
 // Adds event listeners to each image. Must be called after addImagesToPage because addImagesToPage needs to switch out images if there are matching images within ImageObjectsOnPage.
 function initializeEventListener() {
-  for (var i = 0; i < document.getElementsByClassName('researchImage').length; i++) {
-    document.getElementsByClassName('researchImage')[i].addEventListener('click', handleImageClick);
+  for (var i = 0; i < document.getElementsByClassName('research-image').length; i++) {
+    document.getElementsByClassName('research-image')[i].addEventListener('click', handleImageClick);
   }
 };
 
-// Targets image classes on the HTML page and changes the src value to the filepath of the image object returned by the getRandomImage function, one at a time. Returns an array of the names of these image objects on the page in an array called imagesOnPage, so that they can later be identified. Also returns an array of the image objects so that their timesClicked properties can be accessed and incremented when clicked.
+// Targets image classes on the HTML page and changes the src values. Returns an array of the names of these image objects on the page. Also returns an array of the image objects so that their timesClicked properties can be accessed and incremented when clicked.
 function addImagesToPage() {
   // Gets random image object for each img element on the page and pushes both the object itself and the object's name into separate arrays.
-  for (var i = 0; i < document.getElementsByClassName('researchImage').length; i++) {
+  for (var i = 0; i < document.getElementsByClassName('research-image').length; i++) {
     var randomImage = getRandomImage();
     imageNamesOnPage.push(randomImage.name);
     imageObjectsOnPage.push(randomImage);
   }
-  // If any of the objects in the imageObjectsOnPage array match (if two images are the same on the page), replace the reference object with a new random object.
+  // If any of the objects in the imageObjectsOnPage array match replace the reference object with a new random object.
   for (var i = 0; i < imageObjectsOnPage.length; i++) {
     for (var j = 0; j < imageObjectsOnPage.length; j++) {
       if (imageObjectsOnPage[i] === imageObjectsOnPage[j] && i !== j) {
@@ -102,8 +91,8 @@ function addImagesToPage() {
     }
   }
   // Add the appropriate filepath to the image object and update each img element's src on the page
-  for (var i = 0; i < document.getElementsByClassName('researchImage').length; i++) {
-    document.getElementsByClassName('researchImage')[i].src = imageObjectsOnPage[i].filepath;
+  for (var i = 0; i < document.getElementsByClassName('research-image').length; i++) {
+    document.getElementsByClassName('research-image')[i].src = imageObjectsOnPage[i].filepath;
   }
   return imageNamesOnPage, imageObjectsOnPage;
 }
@@ -116,6 +105,18 @@ function ImageObject(name, filepath) {
   this.timesClicked = 0;
   imageObjectArray.push(this);
 }
+
+// creates new data set object for use in canvas graph
+function CreateDataSet(information, fillColor, highlightFill, highlightStroke, displayData) {
+  this.information = information;
+  this.fillColor = fillColor;
+  this.highlightFill = highlightFill;
+  this.highlightStroke = highlightStroke;
+  this.displayData = displayData;
+};
+
+var timesClickedDataSet = new CreateDataSet('Times Clicked', 'rgba(220,220,220,0.5)', 'rgba(220,220,220,0.75)', 'rgba(220,220,220,1)', allTimesClicked);
+var timesShownDataSet = new CreateDataSet('Times Shown', 'rgba(220,220,220,0.5)', 'rgba(220,220,220,0.75)', 'rgba(220,220,220,1)', allTimesShown);
 
 // Creates new object for each image
 var bagImage = new ImageObject('bag', 'img/bag.jpg');
